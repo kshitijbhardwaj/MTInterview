c7d626486f6ca33723b74a7ee2bf25e033220595
kshitijbhardwaj <kshtj21@hotmail.com>
2018-04-13 16:41:13 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import {selector} from 'rxjs/operator/publish';
+import {AppService} from './service/app.service';
+import { JsonPipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AppService]
 })
 export class AppComponent {
   title = 'app';
+  searchText = '';
+  temp: any;
+  showData: any;
+  constructor(private _appservice: AppService ) {}
+  getSearchData() {
+    this.showData = '';
+    this.temp = [];
+    this._appservice.getDetails().subscribe(result => {
+      console.log('result', JSON.stringify(result));
+      result['list'].forEach(data => {
+      this.temp.push(data.main);
+      });
+      console.log('this.temp', this.temp);
+     });
+  }
+
+  getDetails(t) {
+    this.showData = {};
+    this.showData = t;
+    console.log('showData', this.showData);
+  }
 }
---
+++ src/app/service/app.service.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by kshitij on 13/4/18.
+ */
+import {Component, Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class AppService {
+  constructor(private _http: HttpClient) {
+  }
+  getDetails() {
+   return this._http.get('http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22').map(res => res);
+  }
+}
